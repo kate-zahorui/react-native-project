fde44f5908e1b00cb99936ad84df361e1cab8438
kate-zahorui <kate.zahorui@gmail.com>
2022-12-23 19:08:53 +0200

    Creates main screens

---
+++ screens/main/index.js
@@ -0,0 +1,4 @@
+export { default as CommentsScreen } from "./CommentsScreen";
+export { default as CreatePostsScreen } from "./CreatePostsScreen";
+export { default as PostsScreen } from "./PostsScreen";
+export { default as ProfileScreen } from "./ProfileScreen";

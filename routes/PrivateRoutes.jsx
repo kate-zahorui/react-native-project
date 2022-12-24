b3f64b914ef424b434050a9c7d429737ab953ca4
kate-zahorui <kate.zahorui@gmail.com>
2022-12-24 16:00:44 +0200

    Adds icons and styles for bottom navigation

--- routes/PrivateRoutes.jsx
+++ routes/PrivateRoutes.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { SimpleLineIcons, AntDesign, Feather } from "@expo/vector-icons";
+import { StyleSheet } from "react-native";
 
 import { CreatePostsScreen, PostsScreen, ProfileScreen } from "../screens/main";
 
@@ -7,12 +9,76 @@ const MainTab = createBottomTabNavigator();
 
 const PrivateRoutes = () => {
   return (
-    <MainTab.Navigator initialRouteName="Posts" backBehavior="initialRoute ">
-      <MainTab.Screen name="Posts" component={PostsScreen} />
-      <MainTab.Screen name="CreatePosts" component={CreatePostsScreen} />
-      <MainTab.Screen name="Profile" component={ProfileScreen} />
+    <MainTab.Navigator
+      initialRouteName="Публикации"
+      backBehavior="initialRoute"
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: styles.container,
+        tabBarItemStyle: styles.item,
+        tabBarActiveBackgroundColor: "#FF6C00",
+      }}
+    >
+      <MainTab.Screen
+        name="Публикации"
+        component={PostsScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <SimpleLineIcons
+              name="grid"
+              size={24}
+              color={focused ? "#ffffff" : "#212121"}
+            />
+          ),
+        }}
+      />
+      <MainTab.Screen
+        name="Создать публикацию"
+        component={CreatePostsScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <AntDesign
+              name="plus"
+              size={24}
+              color={focused ? "#ffffff" : "#212121"}
+            />
+          ),
+        }}
+      />
+      <MainTab.Screen
+        name="Профиль"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Feather
+              name="user"
+              size={24}
+              color={focused ? "#ffffff" : "#212121"}
+            />
+          ),
+          headerShown: false,
+        }}
+      />
     </MainTab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    minHeight: 65,
+    backgroundColor: "#ffffff",
+    boxShadow: "0px -0.5px 0px rgba(0, 0, 0, 0.3)",
+  },
+  item: {
+    marginBottom: 10,
+    marginTop: 10,
+    marginLeft: 8,
+    marginRight: 8,
+    height: 40,
+    maxWidth: 70,
+    borderRadius: 20,
+  },
+});
 
 export default PrivateRoutes;

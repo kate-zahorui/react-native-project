eaa396bcdbbbf7665e6206a223c6752e1948bb57
kate-zahorui <kate.zahorui@gmail.com>
2022-12-27 09:51:50 +0200

    Fixes navigation

--- routes/PrivateRoutes.jsx
+++ routes/PrivateRoutes.jsx
@@ -1,89 +1,38 @@
 import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { SimpleLineIcons, AntDesign, Feather } from "@expo/vector-icons";
-import { StyleSheet } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { CreatePostsScreen } from "../screens/main";
-import HomeNavigator from "./HomeNavigator";
-import ProfileNavigator from "./ProfileNavigator";
+import { CommentsScreen, MapScreen } from "../screens/main";
+import BottomTabsNavigator from "./BottomTabsNavigator";
 
-const MainTab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 const PrivateRoutes = () => {
   return (
-    <MainTab.Navigator
-      initialRouteName="Home"
-      backBehavior="initialRoute"
-      screenOptions={{
-        tabBarShowLabel: false,
-        tabBarStyle: styles.container,
-        tabBarItemStyle: styles.item,
-        tabBarActiveBackgroundColor: "#FF6C00",
-        tabBarHideOnKeyboard: true,
-      }}
-    >
-      <MainTab.Screen
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
         name="Home"
-        component={HomeNavigator}
+        component={BottomTabsNavigator}
         options={{
-          tabBarIcon: ({ focused }) => (
-            <SimpleLineIcons
-              name="grid"
-              size={24}
-              color={focused ? "#ffffff" : "#212121"}
-            />
-          ),
           headerShown: false,
         }}
       />
-      <MainTab.Screen
-        name="Создать публикацию"
-        component={CreatePostsScreen}
+      <Stack.Screen
+        name="Комментарии"
+        component={CommentsScreen}
         options={{
           tabBarStyle: { display: "none" },
-          tabBarIcon: ({ focused }) => (
-            <AntDesign
-              name="plus"
-              size={24}
-              color={focused ? "#ffffff" : "#212121"}
-            />
-          ),
+          headerTitleAlign: "center",
         }}
       />
-      <MainTab.Screen
-        name="Профиль"
-        component={ProfileNavigator}
+      <Stack.Screen
+        name="Карта"
+        component={MapScreen}
         options={{
-          tabBarIcon: ({ focused }) => (
-            <Feather
-              name="user"
-              size={24}
-              color={focused ? "#ffffff" : "#212121"}
-            />
-          ),
-          headerShown: false,
+          headerTitleAlign: "center",
         }}
       />
-    </MainTab.Navigator>
+    </Stack.Navigator>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    minHeight: 65,
-    backgroundColor: "#ffffff",
-    boxShadow: "0px -0.5px 0px rgba(0, 0, 0, 0.3)",
-  },
-  item: {
-    marginBottom: 10,
-    marginTop: 10,
-    marginLeft: 8,
-    marginRight: 8,
-    height: 40,
-    maxWidth: 70,
-    borderRadius: 20,
-  },
-});
 
 export default PrivateRoutes;
